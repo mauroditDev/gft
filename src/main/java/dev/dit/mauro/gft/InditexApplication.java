58a79884576ed26f1f3e4ca171f60c674110d046
mtullio <mtullio@redpoints.com>
2024-09-06 13:13:32 +0200

    added mvn

---
+++ src/main/java/dev/dit/mauro/gft/InditexApplication.java
@@ -0,0 +1,13 @@
+package dev.dit.mauro.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dit/mauro/gft/InditexApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dit.mauro.gft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
